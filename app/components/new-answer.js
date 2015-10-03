15ca39de23538a9645ea8956406db6ef593cde09
alisa <alisaclone@gmail.com>
2015-10-03 15:59:43 -0700

    add the ability to delete questions

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+    saveAnswer() {
+      var params = {
+        responder: this.get('responder'),
+        response: this.get('response'),
+        question: this.get('question')
+      };
+      this.set('addNewAnswer', false),
+      this.sendAction('saveAnswer', params);
+    }
+  }
 });
