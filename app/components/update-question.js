a4e8acee4b4a12a6d801fc2636d27f4f09251c73
alisa <alisaclone@gmail.com>
2015-10-04 10:00:06 -0700

    add component to update question

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -6,6 +6,14 @@ export default Ember.Component.extend({
     updateQuestionForm() {
       this.set('updateQuestionForm', true);
     },
-
+    update(question) {
+      var params = {
+        quester: this.get('quester'),
+        quest: this.get('quest'),
+        notes: this.get('notes'),
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('update', question, params);
+    }
   }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -4,18 +4,16 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
-
   actions: {
     update(question, params) {
       Object.keys(params).forEach(function(key) {
-        if(params[key]) !== undefined) {
+        if(params[key] !== undefined) {
           question.set(key, params[key]);
         }
       });
       question.save();
       this.transitionTo('question', params.question);
     },
-
     saveAnswer(params) {
       var newAnswer = this.store.createRecord('answer', params);
       newAnswer.save();
